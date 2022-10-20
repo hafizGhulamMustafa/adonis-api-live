8b16a2ab2f08fe48740eee2885cb194268963a77
Ghulam-Mustafa <themustafadeveloper@gmail.com>
2022-10-20 16:56:35 +0500

    git init

--- app/Controllers/Http/TasksController.ts
+++ app/Controllers/Http/TasksController.ts
@@ -2,6 +2,6 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class TasksController {
     public async index({response}:HttpContextContract){
-        return response.send('hello this is working')
+        return response.send('hello this route is working')
     }
 }
